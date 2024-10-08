b9f8b577e332d75b60334bbef56dc01906e8f3a9
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2024-10-08 23:12:10 +0200

    Fixed current status not showing

--- components/quickstart/teacherverification.tsx
+++ components/quickstart/teacherverification.tsx
@@ -3,6 +3,6 @@ import { User } from "@prisma/client";
 export async function VerifyRequest({user}: {user: User}){
     return (<>
         This page is work in progress. Make sure to check back later.
-        <p> Your current status is {user.teacherVerified}</p>
+        <p> Your current status is {user.role}</p>
     </>)
 }
