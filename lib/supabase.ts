6467bdab1067c57ad924baf1dd002c9f19c6eafc
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2024-10-19 20:42:12 +0200

    env name changed

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 // Create Supabase client
-export const supabase = createClient(process.env.SUPABASE_URL as string, process.env.SUPABASE_API_KEY as string)
+export const supabase = createClient(process.env.SUPABASE_STORAGE_URL as string, process.env.SUPABASE_API_KEY as string)
 
 // Upload file using standard upload
 export async function uploadFile() {
