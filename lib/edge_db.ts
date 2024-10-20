476d7934fb4191ef903150faced38b4e927373d0
Benceszalaiii <120375165+Benceszalaiii@users.noreply.github.com>
2024-10-20 15:43:20 +0200

    Update edge_db.ts

--- lib/edge_db.ts
+++ lib/edge_db.ts
@@ -1,6 +1,13 @@
 import { prisma } from "@lib/prisma"
-
+export const runtime = "edge";
 export async function getAllClasses(){
     const classes = await prisma.class.findMany();
     return classes
+}
+export async function getUserById(id: string): Promise<User | null> {
+  const user = await prisma.user.findUnique({ where: { id: id } });
+  if (!user) {
+    return null;
+  }
+  return user;
 }
